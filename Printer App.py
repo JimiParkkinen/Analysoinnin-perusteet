9a66d23109e3fbfe39aa3918b800d856200a920c
JimiParkkinen <jimi.parkkinen@gmail.com>
2023-11-08 17:46:52 +0200

    Printer App

---
+++ Printer App.py
@@ -0,0 +1,26 @@
+Python 3.12.0 (tags/v3.12.0:0fb18b0, Oct  2 2023, 13:03:39) [MSC v.1935 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> print ("Printer App")
+Printer App
+>>> print ("This app is made by")
+This app is made by
+>>> creator = "Jimi"
+>>> print ("This app is made by", creator)
+This app is made by Jimi
+>>> number1 = 10
+>>> number2 = 5
+# >>> print (number1)
+# 10
+>>> print ("The value of number1 is", number1)
+The value of number1 is 10
+>>> print ("The value of number2 is", number2)
+The value of number2 is 5
+>>> multiplication = (number1 * number2)
+>>> print (multiplication)
+50
+>>> print (number1 + number2)
+15
+>>> print (number1 - number2)
+5
+>>> print (number1 / number2)
+2.0
